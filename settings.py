2ca3a9f176a837bd7527ef2ad29f8df76b1e7e13
Eric <errkkgeorge+github@gmail.com>
2012-05-29 21:28:51 +0100

    tweaked for proper venv

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -81,7 +81,6 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
-    os.path.join(SITE_ROOT, 'images'),
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'fbapp.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- urls.py
+++ urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
-from fbapp.fb.views import home
+from fb.views import home
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    
     url(r'^$', home, name='home'),
-    url(r'^fbapp/', include('fbapp.fb.urls')),
+    url(r'^fbapp/', include('fb.urls')),
 
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^facebook/', include('django_facebook.urls')),
-		url(r'^accounts/', include('django_facebook.auth_urls')),
+	url(r'^accounts/', include('django_facebook.auth_urls')),
 )
