9b289d4775c3bfc18ea8c1b198a80222e658d807
Eric <errkkgeorge@gmail.com>
2012-05-30 13:01:21 +0100

    removing static reference in settings

--- settings.py
+++ settings.py
@@ -82,7 +82,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'static'),
+    # os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
